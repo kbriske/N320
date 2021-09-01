fe9c44492ebe618ebf1625172562b4dd3b5d2efd
kbriske <70216437+kbriske@users.noreply.github.com>
2021-09-01 09:13:21 -0400

    changed some things to make room

--- testing/js/app.js
+++ testing/js/app.js
@@ -23,36 +23,35 @@
 
 //P5
 
-class Circle {
-    constructor(circleX, circleY, radius, color) {
-        this.x = circleX;
-        this.y = circleY;
-        this.radius = radius;
-        this.color = color;
-        this.speed = 1 + Math.random() * 2;
-    }
-    update() {
-        this.y +=this.speed;
-        this.speed = this.speed + .1;
-        fill(this.color);
-        circle(this.x,this.y, this.radius);
-    }
-}
+// class Circle {
+//     constructor(circleX, circleY, radius, color) {
+//         this.x = circleX;
+//         this.y = circleY;
+//         this.radius = radius;
+//         this.color = color;
+//         this.speed = 1 + Math.random() * 2;
+//     }
+//     update() {
+//         this.y +=this.speed;
+//         this.speed = this.speed + .1;
+//         fill(this.color);
+//         circle(this.x,this.y, this.radius);
+//     }
+// }
 
-let myCircles = [];
-myCircles[0] = new Circle(100,50,30,[10, 82, 250])
-myCircles[1] = new Circle(300,50,30,[102, 148, 255])
+// let myCircles = [];
+// myCircles[0] = new Circle(100,50,30,[10, 82, 250])
+// myCircles[1] = new Circle(300,50,30,[102, 148, 255])
 
 
-function setup() {
-    createCanvas(400,400);
-}
+// function setup() {
+//     createCanvas(400,400);
+// }
 
-function draw() {
-    background(0,0,0);
+// function draw() {
+//     background(0,0,0);
 
-    myCircles[0].update();
-    myCircles[1].update();
-}
-
+//     myCircles[0].update();
+//     myCircles[1].update();
+// }
 
